812b291131d165b0034402c33004a31c4256c6f7
tarikopenclassrooms <maarijtarik@hotmail.com>
2023-10-23 19:24:54 +0200

    Employee list

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,105 +1,113 @@
-import React, {useEffect } from 'react';
-import Box from '@mui/material/Box';
-import { DataGrid } from '@mui/x-data-grid';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { loadFromLocalStorage } from '../store/employeeSlice';
+import React, { useEffect } from "react";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { loadFromLocalStorage } from "../store/employeeSlice";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90, hideable: true },
   {
-    field: 'firstName',
-    headerName: 'First name',
+    field: "firstName",
+    headerName: "First name",
     width: 150,
     editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
+    field: "lastName",
+    headerName: "Last name",
     width: 150,
     editable: true,
   },
   {
-    field: 'startDate',
-    headerName: 'Start Date',
+    field: "startDate",
+    headerName: "Start Date",
     width: 110,
     editable: true,
   },
   {
-    field: 'department',
-    headerName: 'Department',
+    field: "department",
+    headerName: "Department",
     width: 110,
     editable: true,
   },
   {
-        field: 'dateOfBirth',
-        headerName: 'Date of Birth',
-        width: 110,
-        editable: true,
-      },
-      {
-        field: 'street',
-        headerName: 'Street',
-        width: 110,
-        editable: true,
-      },
-      {
-        field: 'city',
-        headerName: 'City',
-        width: 110,
-        editable: true,
-      },
-      {
-        field: 'state',
-        headerName: 'State',
-        width: 110,
-        editable: true,
-      },
-      {
-        field: 'zipCode',
-        headerName: 'Zip Code',
-        width: 110,
-        editable: true,
-      },
+    field: "dateOfBirth",
+    headerName: "Date of Birth",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "street",
+    headerName: "Street",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "state",
+    headerName: "State",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "zipCode",
+    headerName: "Zip Code",
+    width: 110,
+    editable: true,
+  },
 ];
 
-
-
-
+/**
+ * Table  with the employee list
+ * @returns JSX with HTML elements
+ */
 const EmployeeList = () => {
-  const dispatch = useDispatch()
-  const employees = useSelector(state => state.employees.list)
-  console.log("Lista desde empleados", employees)
+  const dispatch = useDispatch();
+  const employees = useSelector((state) => state.employees.list);
+  console.log("Lista desde empleados", employees);
 
-useEffect(()=>{
-    if (localStorage.getItem('employee_list') && employees.length === 0){
-      console.log("Localstorage", localStorage.getItem('employee_list'))
-      dispatch(loadFromLocalStorage())
+  useEffect(() => {
+    if (localStorage.getItem("employee_list") && employees.length === 0) {
+      console.log("Localstorage", localStorage.getItem("employee_list"));
+      dispatch(loadFromLocalStorage());
     }
-  }, [])
+  }, []);
 
   return (
-<>
-    <Box sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        /*getRowId={(row) => row.internalId}*/
-        rows={employees}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
+    <>
+      <Header />
+      <Box sx={{ height: 400, width: "100%", color: "white" }}>
+        <DataGrid
+          style={{backgroundColor: 'white'}}
+          rows={employees}
+          columns={columns}
+          
+          initialState={{
+            columnVisibilityModel: {
+              id: false,
             },
-          },
-        }}
-        pageSizeOptions={[5]}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </Box>
-    <Link to={'/'}>Home</Link>
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
+      <Link className="button" to={"/"}>Home</Link>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default EmployeeList
+export default EmployeeList;
